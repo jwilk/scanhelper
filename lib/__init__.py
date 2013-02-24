9b77182d8d8dbc23fe4eaab848986e21bbca9e3d
Jakub Wilk <jwilk@jwilk.net>
2013-02-24 18:05:03 +0100

    Check Python version at runtime.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,1 +1,10 @@
 __version__ = '0.2.4'
+
+import sys
+
+if sys.version_info < (2, 6):
+    raise RuntimeError('Python >= 2.6 is required')
+if sys.version_info >= (3, 0):
+    raise RuntimeError('Python 2.X is required')
+
+# vim:ts=4 sw=4 et
