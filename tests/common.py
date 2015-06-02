01483b1eef7003369cce937feb444735f6ab5668
Jakub Wilk <jwilk@jwilk.net>
2015-06-02 20:21:05 +0200

    tests: add.

---
+++ tests/common.py
@@ -0,0 +1,136 @@
+# encoding=UTF-8
+
+# Copyright © 2010-2015 Jakub Wilk <jwilk@jwilk.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+import contextlib
+import functools
+import os
+import re
+import sys
+import traceback
+
+from nose import SkipTest
+from nose.tools import (
+    assert_equal,
+)
+
+def assert_regexp_matches(regexp, text):
+    if isinstance(regexp, basestring):
+        regexp = re.compile(regexp)
+    if not regexp.search(text):
+        message = "regexp doesn't match: {0!r} not found in {1!r}".format(regexp.pattern, text)
+        raise AssertionError(message)
+
+def assert_rfc3339_timestamp(timestamp):
+    return assert_regexp_matches(
+        '^[0-9]{4}(-[0-9]{2}){2}T[0-9]{2}(:[0-9]{2}){2}([+-][0-9]{2}:[0-9]{2}|Z)$',
+        timestamp
+    )
+
+@contextlib.contextmanager
+def interim_environ(**override):
+    keys = set(override)
+    copy_keys = keys & set(os.environ)
+    copy = dict((key, value) for key, value in os.environ.iteritems() if key in copy_keys)
+    for key, value in override.iteritems():
+        if value is None:
+            try:
+                del os.environ[key]
+            except KeyError:
+                pass
+        else:
+            os.environ[key] = value
+    try:
+        yield
+    finally:
+        for key in keys:
+            try:
+                del os.environ[key]
+            except KeyError:
+                pass
+        os.environ.update(copy)
+
+class IsolatedError(Exception):
+    pass
+
+def _n_relevant_tb_levels(tb):
+    n = 0
+    while tb and '__unittest' not in tb.tb_frame.f_globals:
+        n += 1
+        tb = tb.tb_next
+    return n
+
+def fork_isolation(f):
+
+    EXIT_EXCEPTION = 101
+    EXIT_SKIP_TEST = 102
+
+    exit = os._exit
+    # sys.exit() can't be used here, because nose catches all exceptions,
+    # including SystemExit
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        readfd, writefd = os.pipe()
+        pid = os.fork()
+        if pid == 0:
+            os.close(readfd)
+            try:
+                f(*args, **kwargs)
+            except SkipTest as exc:
+                s = str(exc)
+                with os.fdopen(writefd, 'wb') as fp:
+                    fp.write(s)
+                exit(EXIT_SKIP_TEST)
+            except Exception:
+                exctp, exc, tb = sys.exc_info()
+                s = traceback.format_exception(exctp, exc, tb, _n_relevant_tb_levels(tb))
+                s = ''.join(s)
+                del tb
+                with os.fdopen(writefd, 'wb') as fp:
+                    fp.write(s)
+                exit(EXIT_EXCEPTION)
+            exit(0)
+        else:
+            os.close(writefd)
+            with os.fdopen(readfd, 'rb') as fp:
+                msg = fp.read()
+            msg = msg.rstrip('\n')
+            pid, status = os.waitpid(pid, 0)
+            if status == (EXIT_EXCEPTION << 8):
+                raise IsolatedError('\n\n' + msg)
+            elif status == (EXIT_SKIP_TEST << 8):
+                raise SkipTest(msg)
+            elif status == 0 and msg == '':
+                pass
+            else:
+                raise RuntimeError('unexpected isolated process status {0}'.format(status))
+
+    return wrapper
+
+if 'coverage' in sys.modules:
+    del fork_isolation  # quieten pyflakes
+    def fork_isolation(f):
+        # Fork isolation would break coverage measurements.
+        # Oh well. FIXME.
+        return f
+
+__all__ = [
+    'SkipTest',
+    'assert_equal',
+    'assert_regexp_matches',
+    'assert_rfc3339_timestamp',
+    'fork_isolation',
+    'interim_environ',
+]
+
+# vim:ts=4 sts=4 sw=4 et
---
+++ tests/test_xmp.py
@@ -0,0 +1,52 @@
+# encoding=UTF-8
+
+# Copyright © 2012-2015 Jakub Wilk <jwilk@jwilk.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+import time
+
+from . common import (
+    assert_equal,
+    assert_rfc3339_timestamp,
+    fork_isolation,
+    interim_environ,
+)
+
+from lib import xmp
+
+def test_now():
+    ts = xmp.now()
+    assert_rfc3339_timestamp(str(ts))
+
+def test_mtime():
+    ts = xmp.mtime(__file__)
+    assert_rfc3339_timestamp(str(ts))
+
+def test_timezones():
+    @fork_isolation
+    def t(uts, tz, expected):
+        with interim_environ(TZ=tz):
+            time.tzset()
+            ts = xmp.rfc3339(uts)
+            assert_rfc3339_timestamp(str(ts))
+            assert_equal(str(ts), expected)
+    # non-DST:
+    t(1261171514, 'UTC', '2009-12-18T21:25:14+00:00')
+    t(1261171514, 'Europe/Warsaw', '2009-12-18T22:25:14+01:00')
+    t(1261171514, 'America/New_York', '2009-12-18T16:25:14-05:00')
+    t(1261171514, 'Asia/Kathmandu', '2009-12-19T03:10:14+05:45')
+    t(1261171514, 'HAM+4:37', '2009-12-18T16:48:14-04:37')
+    # DST:
+    t(1337075844, 'Europe/Warsaw', '2012-05-15T11:57:24+02:00')
+    # Offset changes:
+    t(1394737792, 'Europe/Moscow', '2014-03-13T23:09:52+04:00')  # used to be +04:00, but it's +03:00 now
+
+# vim:ts=4 sts=4 sw=4 et
