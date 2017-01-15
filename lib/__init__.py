69d1dd861ef26dc88b211c5c5132d605ff671d58
Jakub Wilk <jwilk@jwilk.net>
2017-01-15 17:12:07 +0100

    Move __version__ downwards.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,10 +1,10 @@
-__version__ = '0.6.2'
-
 import sys
 
 if sys.version_info < (2, 6):
     raise RuntimeError('Python >= 2.6 is required')
 if sys.version_info >= (3, 0):
     raise RuntimeError('Python 2.X is required')
+
+__version__ = '0.6.2'
 
 # vim:ts=4 sts=4 sw=4 et
