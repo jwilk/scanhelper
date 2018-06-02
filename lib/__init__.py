75fc7718f4154e6ffc00f833c87b4140174dff63
Jakub Wilk <jwilk@jwilk.net>
2018-06-02 22:49:32 +0200

    Add missing copyright notices.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,18 @@
+# encoding=UTF-8
+
+# Copyright © 2011-2018 Jakub Wilk <jwilk@jwilk.net>
+#
+# This file is part of scanhelper.
+#
+# scanhelper is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# scanhelper is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+
 '''
 scanhelper's private modules
 '''
